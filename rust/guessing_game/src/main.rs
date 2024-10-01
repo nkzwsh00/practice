1de1bb79ab913442cf0414d5ea7e3eefb1ba16b0
snakazawa <shuntaro.n@gmail.com>
2024-10-01 21:37:32 +0900

    Refactor: Add user input loop to guessing game

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -5,16 +5,18 @@ fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101);
     println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!"); //数値を入力してください！
-    println!("You guessed: {}", guess);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),  //小さすぎ！
-        Ordering::Greater => println!("Too big!"), //大きすぎ！
-        Ordering::Equal => println!("You win!"),   //やったね！
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!"); //数値を入力してください！
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),  //小さすぎ！
+            Ordering::Greater => println!("Too big!"), //大きすぎ！
+            Ordering::Equal => println!("You win!"),   //やったね！
+        }
     }
 }
