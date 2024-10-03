5e3ca6b6fe78f86281c2af2c87a8296bea34226e
snakazawa <shuntaro.n@gmail.com>
2024-10-03 22:04:00 +0900

    Refactor: Comment out unused code in main.rs

--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
-    let x = 5;
+    // let x = 5;
 
-    let x = x + 1;
+    // let x = x + 1;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {}", x);
+    // }
 
-    println!("The value of x is: {}", x);
+    // println!("The value of x is: {}", x);
+    let guess: String = "42".parse().expect("Not a number!");
 }
